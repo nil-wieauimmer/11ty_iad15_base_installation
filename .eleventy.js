2a2b01a476e1ba8a75eef96ca1972497df44587d
Nil Saegesser <nil.saegesser@gmail.com>
2025-11-07 15:22:32 +0100

    link fix

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
         dir: {
             input: "src",
             output: "docs"
-        }
+        },
+        pathPrefix: process.env.ELEVENTY_ENV === "production" ? "/11ty_iad15_base_installation/" : "/"
     };
 }
